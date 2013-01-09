da92523dd993bded034b9a83c14d9306eb5b933d
Nick Phillips <x1nick@ubuntu.(none)>
2013-01-09 23:25:30 +0000

    Initial development

---
+++ cli.php
@@ -0,0 +1,2 @@
+<?php
+echo "This has executed PHP!";
---
+++ server.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <libconfig.h>
+
+int main(int argc, char* argv[])
+{
+  config_t cfg;
+  config_setting_t *setting;
+  const char *cmd = "";
+  int port;
+
+  char *config_file_name = "server.cfg";
+
+  /*Initialization */
+  config_init(&cfg);
+
+  /* Read the file. If there is an error, report it and exit. */
+  if (!config_read_file(&cfg, config_file_name))
+  {
+    printf("Error unable to process config file\n\n");
+    config_destroy(&cfg);
+    return -1;
+  }
+
+  if (config_lookup_int(&cfg, "port", &port))
+  {
+    printf("\nPort to run server on: %d", port);
+  } else {
+
+  }
+
+  if (config_lookup_string(&cfg, "cmd", &cmd))
+  {
+    printf("\nCommand to execute for events: %s", cmd);
+  } else {
+    cmd = "";
+  }
+
+
+  return 0;
+}
